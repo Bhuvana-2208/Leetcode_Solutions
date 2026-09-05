7e5075feadf26bf47cc0ca4ef4c56a2dd07783c6
NEELA BHUVANAKRUTHI <274014088+Bhuvana-2208@users.noreply.github.com>
2026-09-05 23:10:47 +0530

    Time: 5 ms (66.41%), Space: 133.1 MB (27.86%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+       
+        int rightSum[]=new int[n];
+         
+         rightSum[n-1]=nums[n-1];
+         for(int i=n-2;i>=0;i--)
+         {
+            rightSum[i]=Math.min(rightSum[i+1],nums[i]);
+         }
+         int left=nums[0];
+         for(int i=0;i<n;i++)
+         {
+             left=Math.max(left,nums[i]);
+            int instability=left-rightSum[i];
+            if(instability<=k)
+            {
+                return i;
+            }
+         }
+         return -1;
+
+        
+    }
+}
