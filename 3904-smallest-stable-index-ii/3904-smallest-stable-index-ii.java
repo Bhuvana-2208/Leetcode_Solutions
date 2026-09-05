5b5fad9421809e8d6a42714b334a0f050d746f6c
NEELA BHUVANAKRUTHI <274014088+Bhuvana-2208@users.noreply.github.com>
2026-09-05 23:17:44 +0530

    Time: 4 ms (88.55%), Space: 133.4 MB (8.01%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -3,13 +3,13 @@ class Solution {
         int n=nums.length;
        
         int rightSum[]=new int[n];
-         
+         int left=nums[0];
          rightSum[n-1]=nums[n-1];
          for(int i=n-2;i>=0;i--)
          {
             rightSum[i]=Math.min(rightSum[i+1],nums[i]);
          }
-         int left=nums[0];
+        
          for(int i=0;i<n;i++)
          {
              left=Math.max(left,nums[i]);
