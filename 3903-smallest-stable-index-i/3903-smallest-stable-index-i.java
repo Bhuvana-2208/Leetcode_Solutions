f66cc6b4b4fbfe8a3e402b2b508712560962d021
NEELA BHUVANAKRUTHI <274014088+Bhuvana-2208@users.noreply.github.com>
2026-09-05 22:38:37 +0530

    Time: 12 ms (4.63%), Space: 46.7 MB (15.79%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            int max = Arrays.stream(nums, 0, i+1).max().getAsInt();
+            int min = Arrays.stream(nums, i, n).min().getAsInt();
+            int instability=max-min;
+            if(instability<=k)
+            {
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+}
